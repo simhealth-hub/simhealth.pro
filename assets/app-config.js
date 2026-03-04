217a8f79d3224494d6f084a43f9f53ba8ddfe943
simhealth-hub <founder@simhealth.pro>
2026-03-04 19:25:26 +0100

    Create app-config.js

---
+++ assets/app-config.js
@@ -0,0 +1,10 @@
+// assets/app-config.js
+window.SIMHEALTH = window.SIMHEALTH || {};
+
+window.SIMHEALTH.ENDPOINTS = {
+  // Website signups / updates / pilot / contact script:
+  SIGNUPS_EXEC: "https://script.google.com/macros/s/AKfycbxJ1xiM2CKY_H4KQRpcJFqjXXUjWKecK_K8Uyqv4AIgeV2uhj3iNjmNk7iJK9CxRTDQ/exec",
+
+  // Questionnaire script (only if you want it here too later):
+  // QUESTIONNAIRE_EXEC: "https://script.google.com/macros/s/XXXX/exec",
+};
