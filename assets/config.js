b7cacf37ba37009aa205113d005d7f0ecae8ce1d
simhealth-hub <founder@simhealth.pro>
2026-03-04 17:25:13 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.SIMHEALTH_ENDPOINT = "https://script.google.com/macros/s/AKfycbx3IyymIxfC7ecFkZ045S4bA1RIBo4LPv2xZxftqLOs/dev";
+window.SIMHEALTH_ENDPOINT = "https://script.google.com/macros/s/AKfycbxJ1xiM2CKY_H4KQRpcJFqjXXUjWKecK_K8Uyqv4AIgeV2uhj3iNjmNk7iJK9CxRTDQ/exec";
