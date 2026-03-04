50e08eb5ee0aa3473235bcb0695551ec887959e3
simhealth-hub <founder@simhealth.pro>
2026-03-04 19:45:07 +0100

    Update app-config.js

--- assets/app-config.js
+++ assets/app-config.js
@@ -1,7 +1,7 @@
-// assets/app-config.js
-
+// /assets/app-config.js
 window.SIMHEALTH = window.SIMHEALTH || {};
 
 window.SIMHEALTH.ENDPOINTS = {
+  // Website signups / updates / pilot / contact:
   SIGNUPS_EXEC: "https://script.google.com/macros/s/AKfycbxJ1xiM2CKY_H4KQRpcJFqjXXUjWKecK_K8Uyqv4AIgeV2uhj3iNjmNk7iJK9CxRTDQ/exec"
 };
