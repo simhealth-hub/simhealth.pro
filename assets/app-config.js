16270c782deebc6090dc1ffad8d26623e3552929
simhealth-hub <founder@simhealth.pro>
2026-03-08 19:00:34 +0100

    Update app-config.js

--- assets/app-config.js
+++ assets/app-config.js
@@ -2,6 +2,6 @@
 window.SIMHEALTH = window.SIMHEALTH || {};
 
 window.SIMHEALTH.ENDPOINTS = {
-  // Website signups / updates / pilot / contact:
-  SIGNUPS_EXEC: "https://script.google.com/macros/s/AKfycbxJ1xiM2CKY_H4KQRpcJFqjXXUjWKecK_K8Uyqv4AIgeV2uhj3iNjmNk7iJK9CxRTDQ/exec"
+  SIGNUPS_EXEC: "https://script.google.com/macros/s/AKfycbxJ1xiM2CKY_H4KQRpcJFqjXXUjWKecK_K8Uyqv4AIgeV2uhj3iNjmNk7iJK9CxRTDQ/exec",
+  QUESTIONNAIRE_EXEC: "https://script.google.com/macros/s/AKfycbxCQhCS2r-q1pZDmIzRZzu-Y-med3Prp-jx_CPfcCSNAj3vdnm_2o5qyHWdr3UW6ScUZA/exec"
 };
